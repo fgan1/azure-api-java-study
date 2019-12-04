89b73b90f52c9301b54daa245ceb40b5392f09e1
fgan1 <fgan1@hotmail.com>
2019-12-04 18:56:25 -0300

    First commit

---
+++ src/main/java/com/fgan/azure/App.java
@@ -0,0 +1,13 @@
+package com.fgan.azure;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
